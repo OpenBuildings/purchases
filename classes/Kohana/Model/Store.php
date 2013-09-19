e94cb5e621aacb8f763f75159cbe9f1a5a1feb9b
Ivan Kerin <ivan@openbuildings.com>
2013-09-19 17:23:09 +0300

    add more associations for store

--- classes/Kohana/Model/Store.php
+++ classes/Kohana/Model/Store.php
@@ -8,6 +8,14 @@ class Kohana_Model_Store extends Jam_Model {
 	public static function initialize(Jam_Meta $meta)
 	{
 		$meta
+			->associations(array(
+				'store_purchases' => Jam::association('hasmany', array(
+					'inverse_of' => 'store'
+				)),
+				'purchases' => Jam::association('manytomany', array(
+					'join_table' => 'store_purchases',
+				)),
+			))
 			->fields(array(
 				'id' => Jam::field('primary'),
 				'name' => Jam::field('string'),
