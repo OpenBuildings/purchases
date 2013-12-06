16d48d17b92b64c17f40e19748053c88fb71384a
Haralan Dobrev <hkdobrev@gmail.com>
2013-12-06 11:31:49 +0200

    Add validator messages for expired cards

---
+++ messages/validators/emp_form.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+	'exp_year' => array(
+		'card_expired' => 'Your card has expired',
+	),
+	'exp_month' => array(
+		'card_expired' => 'Your card has expired',
+	),
+);
